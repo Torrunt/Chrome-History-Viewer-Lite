a7ee7015d6fe4a5a6fb3565c7f5d80ef3a5d4694
Torrunt <me@torrunt.net>
2018-06-22 15:23:28 +0800

    Cleaned up formatting

--- init.js
+++ init.js
@@ -1,25 +1,25 @@
 var default_settings =
 {
-	showTime:true, 
-	showSearch:true,
-	boldFont:false,
-	menuTop:true,
-	numItems:20,
-	numLines:0
+    showTime: true,
+    showSearch: true,
+    boldFont: false,
+    menuTop: true,
+    numItems: 20,
+    numLines: 0
 };
 
 var settings;
 
 // Show |url| in a new tab.
-function showUrl(url,closewindow)
-{ 
-	var index=999999;
+function showUrl(url, closewindow)
+{
+    var index = 999999;
 
-	if (closewindow)
-	{
-		chrome.tabs.create({"url": url, "index": index,"selected":true});  
-		window.close();
-	}
-	else
-		chrome.tabs.create({"url": url, "index": index,"selected":false});  
+    if (closewindow)
+    {
+        chrome.tabs.create({ "url": url, "index": index, "selected": true });
+        window.close();
+    }
+    else
+        chrome.tabs.create({ "url": url, "index": index, "selected": false });
 }
